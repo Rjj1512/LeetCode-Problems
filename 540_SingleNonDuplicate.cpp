804d24fb5d2dd8368c071b5ea18ea9cf0e13587d
Rjj1512 <jhaveri.rahil@gmail.com>
2020-05-12 13:59:32 +0530

    2 problems added

---
+++ 540_SingleNonDuplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size() == 1)
+            return nums[0];
+        int i = 0, j = nums.size()-1;
+        while(i <= j) {
+            if(nums[i] != nums[i+1]) {
+                return nums[i];
+            }
+            else if(nums[j] != nums[j-1]) {
+                return nums[j];
+            }
+            i+=2;
+            j-=2;
+        }
+        return -1;
+    }
+};
